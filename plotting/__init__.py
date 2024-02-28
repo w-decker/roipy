fc9711be19185afcbecadeca8a6b612e7c377a95
Your Name <deckerwill7@gmail.com>
2024-02-28 12:09:31 -0600

    update

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -1,1 +1,2 @@
-from .plotting import plot_multivariate, plot_univariate, plot_whole_multivariate
+from .utils import plot_multivariate, plot_univariate, plot_whole_multivariate
+from .plotting import Plot
