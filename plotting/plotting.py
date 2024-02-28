6340b90103a605b3e5bce5e1551a8ca51329863b
Your Name <deckerwill7@gmail.com>
2024-02-28 12:09:43 -0600

    turn to object and fix som ebugs

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -3,23 +3,17 @@ from nilearn.plotting import plot_surf_roi, show
 import numpy as np
 import matplotlib.figure
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
 
-def plot_univariate(roi: list[int], cmap: str, 
-                    shape: str = 'infl', hemi: str = 'left', 
-                    view: str = 'lateral') -> matplotlib.figure.Figure:
-    """Plot univariate brain
+@dataclass
+class Plot(object):
+    """Plot class
     
-    Parameters
-    ----------
-    roi: list[int]
-        Region(s) of interest defined by Destriuex atlas
-
-    cmap: str
-        Nilearn colormap
-
+    Parameteres
+    -----------
     shape: str
-        Shape of brain surface
-        Options: 'infl', 'pial'
+            Shape of brain surface
+            Options: 'infl', 'pial'
 
     hemi: str
         Hemisphere
@@ -29,169 +23,148 @@ def plot_univariate(roi: list[int], cmap: str,
         which view
         Options: “lateral”, “medial”, “dorsal”, “ventral”,”anterior”, “posterior”
 
-    Return
-    ------
-    P: matplotlib.figure.Figure
-        Return matplotlib object
-    """
-
-    ATLAS = fetch_atlas_surf_destrieux()
-
-    # parse args
-    if hemi == "left":
-        hemi_atlas = 'map_left'
-        hemi_shape = 'left'
-    elif hemi == 'right':
-        hemi_atlas = 'map_right'
-        hemi_shape ='right'
-
-    # parcellation
-    parcel = ATLAS[hemi_atlas]
-
-    # get ROI and create mask
-    mask = (parcel == roi)
-
-    # fsaverage
-    fsaverage = fetch_surf_fsaverage()
-    fscat = shape + '_' + hemi_shape
-
-    # plot
-    P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
-                            hemi=hemi_shape, view=view,
-                            bg_map=fsaverage[f'sulc_{hemi_shape}'], bg_on_data=True,
-                            darkness=.5, cmap=cmap)
-    show()
-
-    return P
-
-def plot_multivariate(roi: list[int], cmap: str,
-                    shape: str = 'infl', hemi: str = 'left', 
-                    view: str = 'lateral') -> matplotlib.figure.Figure:
-    """Plot multivariate brain
-
-    Parameters
+    Attributes
     ----------
-    roi: list[int]
-        Region(s) of interest defined by Destriuex atlas
-
-    cmap: str
-        Nilearn colormap
-
-    shape: str
-        Shape of brain surface
-        Options: 'infl', 'pial'
-
-    hemi: str
-        Hemisphere
-        Options: 'left' or 'l', 'right' or 'r'
-    
-    view: str
-        which view
-        Options: “lateral”, “medial”, “dorsal”, “ventral”,”anterior”, “posterior”
-
-    Return
-    ------
-    P: matplotlib.figure.Figure
-        Return matplotlib object        
+    atlas
+        Destrieux Surface atlas
     """
+    shape: str = 'infl'
+    hemi: str = 'left'
+    view: str = 'lateral'
+    atlas = fetch_atlas_surf_destrieux()
 
-    # atlas
-    ATLAS = fetch_atlas_surf_destrieux()
+    def plot_univariate(self, roi: list[int], cmap: str) -> matplotlib.figure.Figure:
+        """Plot univariate brain
+        
+        Parameters
+        ----------
+        roi: list[int]
+            Region(s) of interest defined by Destriuex atlas
 
-    # get axes
-    plt.cla()
-    plt.axis('off')
+        cmap: str
+            Nilearn colormap
 
-    # parse args
-    if hemi == "left":
-        hemi_atlas = 'map_left'
-        hemi_shape = 'left'
-    elif hemi == 'right':
-        hemi_atlas = 'map_right'
-        hemi_shape ='right'
+        Return
+        ------
+        P: matplotlib.figure.Figure
+            Return matplotlib object
+        """
 
-    #mask   
-    hemi = ATLAS[hemi_atlas]
-    mask = np.zeros_like(hemi, dtype=float)
-    for i in roi:
-        roi_indices = np.where(hemi == i)
-        mask[roi_indices] = np.random.uniform(0.1, 10, size=len(roi_indices[0]))
+        # parse args
+        if self.hemi == "left":
+            self.hemi_atlas = 'map_left'
+            self.hemi_shape = 'left'
+        elif self.hemi == 'right':
+            self.hemi_atlas = 'map_right'
+            self.hemi_shape ='right'
 
-    # fsaverage
-    fsaverage = fetch_surf_fsaverage()
-    fscat = shape + '_' + hemi_shape
+        # parcellation
+        parcel = self.atlas[self.hemi_atlas]
 
-    # Plot
-    P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
-                                hemi=hemi_shape, view=view,
-                                bg_map=fsaverage[f'sulc_{hemi_shape}'], bg_on_data=True,
-                                darkness=0.5, cmap=cmap, figure=plt.gcf())
-    
-    show()
-    
-    return [P]
+        # get ROI and create mask
+        mask = (parcel == roi)
 
-def plot_whole_multivariate(cmap: str,
-                    shape: str = 'infl', hemi: str = 'left', 
-                    view: str = 'lateral') -> matplotlib.figure.Figure:
-    """Plot multivariate whole brain
-    
-    Parameters
-    ----------
-    cmap: str
-        Nilearn colormap
+        # fsaverage
+        fsaverage = fetch_surf_fsaverage()
+        fscat = self.shape + '_' + self.hemi_shape
 
-    shape: str
-        Shape of brain surface
-        Options: 'infl', 'pial'
+        # plot
+        P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
+                                hemi=self.hemi_shape, view=self.view,
+                                bg_map=fsaverage[f'sulc_{self.hemi_shape}'], bg_on_data=True,
+                                darkness=.5, cmap=cmap)
+        show()
 
-    hemi: str
-        Hemisphere
-        Options: 'left' or 'l', 'right' or 'r'
-    
-    view: str
-        which view
-        Options: “lateral”, “medial”, “dorsal”, “ventral”,”anterior”, “posterior”       
+        return [P]
 
-    Return
-    ------
-    P: matplotlib.figure.Figure
-        Return matplotlib object
-    """
+    def plot_multivariate(self, roi: list[int], cmap: str) -> matplotlib.figure.Figure:
+        """Plot multivariate brain
 
-    # get axes
-    plt.cla()
-    plt.axis('off')
+        Parameters
+        ----------
+        roi: list[int]
+            Region(s) of interest defined by Destriuex atlas
 
-    # atlas
-    ATLAS = fetch_atlas_surf_destrieux()
+        cmap: str
+            Nilearn colormap
 
-    # parse args
-    if hemi == "left":
-        hemi_atlas = 'map_left'
-        hemi_shape = 'left'
-    elif hemi == 'right':
-        hemi_atlas = 'map_right'
-        hemi_shape ='right'
+        Return
+        ------
+        P: matplotlib.figure.Figure
+            Return matplotlib object        
+        """
 
-    # fsaverage
-    fsaverage = fetch_surf_fsaverage()
-    fscat = shape + '_' + hemi_shape
+        # get axes
+        plt.cla()
+        plt.axis('off')
 
-    #mask
-    mask = np.zeros_like(ATLAS[hemi_atlas], dtype=float)
-    mask = np.random.uniform(-10, 10, size=np.size(ATLAS[hemi_atlas]))
+        # parse args
+        if self.hemi == "left":
+            self.hemi_atlas = 'map_left'
+            self.hemi_shape = 'left'
+        elif self.hemi == 'right':
+            self.hemi_atlas = 'map_right'
+            self.hemi_shape ='right'
 
-    #plot 
-    P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
-                                hemi=hemi_shape, view=view,
-                                bg_map=fsaverage[f'sulc_{hemi_shape}'], bg_on_data=True,
-                                darkness=.5, cmap=cmap, figure=plt.gcf())
-    
-    return [P]
+        #mask   
+        hemi = self.atlas[self.hemi_atlas]
+        mask = np.zeros_like(hemi, dtype=float)
+        for i in roi:
+            roi_indices = np.where(hemi == i)
+            mask[roi_indices] = np.random.uniform(0.1, 10, size=len(roi_indices[0]))
 
+        # fsaverage
+        fsaverage = fetch_surf_fsaverage()
+        fscat = self.shape + '_' + self.hemi_shape
 
+        # Plot
+        P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
+                                    hemi=self.hemi_shape, view=self.view,
+                                    bg_map=fsaverage[f'sulc_{self.hemi_shape}'], bg_on_data=True,
+                                    darkness=0.5, cmap=cmap, figure=plt.gcf())
+        
+        show()
+        
+        return [P]
 
+    def plot_whole_multivariate(self, cmap: str) -> matplotlib.figure.Figure:
+        """Plot multivariate whole brain
+        
+        Parameters
+        ----------
+        cmap: str
+            Nilearn colormap      
 
+        Return
+        ------
+        P: matplotlib.figure.Figure
+            Return matplotlib object
+        """
 
+        # get axes
+        plt.cla()
+        plt.axis('off')
 
+        # parse args
+        if self.hemi == "left":
+            self.hemi_atlas = 'map_left'
+            self.hemi_shape = 'left'
+        elif self.hemi == 'right':
+            self.hemi_atlas = 'map_right'
+            self.hemi_shape ='right'
+
+        # fsaverage
+        fsaverage = fetch_surf_fsaverage()
+        fscat = self.shape + '_' + self.hemi_shape
+
+        #mask
+        mask = np.zeros_like(self.atlas[self.hemi_atlas], dtype=float)
+        mask = np.random.uniform(-10, 10, size=np.size(self.atlas[self.hemi_atlas]))
+
+        #plot 
+        P = plot_surf_roi(fsaverage[fscat], roi_map=mask,
+                                    hemi=self.hemi_shape, view=self.view,
+                                    bg_map=fsaverage[f'sulc_{self.hemi_shape}'], bg_on_data=True,
+                                    darkness=.5, cmap=cmap, figure=plt.gcf())
+        
+        return [P]
